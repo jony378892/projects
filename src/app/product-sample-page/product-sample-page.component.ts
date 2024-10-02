86a482c1f0f8b3ce51bf18c6c5fbb0a192c2367d
Jony <jony378892@gmail.com>
2024-10-03 01:03:28 +0600

    Product Same functionality add in a component

---
+++ src/app/product-sample-page/product-sample-page.component.ts
@@ -0,0 +1,40 @@
+import { UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-sample-page',
+  standalone: true,
+  imports: [UpperCasePipe],
+  templateUrl: './product-sample-page.component.html',
+  styleUrl: './product-sample-page.component.scss'
+})
+export class ProductSamplePageComponent {
+  arr = ['first', 'jony', 'rony', 'rabiul', 'fariz']
+  touch: boolean = false;
+  currentIndex: number = 0;;
+
+  select(i: number) {
+    this.touch = true;
+    this.currentIndex = i
+    return this.arr[i]
+  }
+
+  next() {
+    this.currentIndex++;
+    if (this.currentIndex == this.arr.length) {
+      this.currentIndex = 0
+    }
+    console.log(this.currentIndex);
+
+
+  }
+
+  previous() {
+    this.currentIndex--;
+    if (this.currentIndex == -1) {
+      this.currentIndex = this.arr.length - 1
+    }
+    console.log(this.currentIndex);
+
+  }
+}
